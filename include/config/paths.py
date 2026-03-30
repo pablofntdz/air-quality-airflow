6ac5aa856a707f714597f73a450ded8261fef5d0
pablofntdz <pfp4298@gmail.com>
2026-03-30 21:09:19 +0200

    Delete config from tracking

--- include/config/paths.py
+++ include/config/paths.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 #Rutas
 BASE_DIR = Path(__file__).resolve().parents[1]
-RAW_DIR  = BASE_DIR / "data2" / "raw"
-TMP_DIR  = BASE_DIR / "data2" / "tmp"
+RAW_DIR  = BASE_DIR / "data" / "raw"
+TMP_DIR  = BASE_DIR / "data" / "tmp"
 MODEL_DIR  = BASE_DIR / "models"
-DATA_HOURLY_DIR  = BASE_DIR / "data2" / "data_hourly"
+DATA_HOURLY_DIR  = BASE_DIR / "data" / "data_hourly"
 
