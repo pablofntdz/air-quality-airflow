28a05671622b8577c6eb59080559d8addd40b73e
pablofntdz <pfp4298@gmail.com>
2026-03-31 18:39:28 +0200

    Include hugging face requirements

--- include/src/download_models.py
+++ include/src/download_models.py
@@ -1,6 +1,7 @@
 from huggingface_hub import hf_hub_download
 from include.config.paths import MODEL_DIR
 import shutil
+import os
 
 REPO_ID = "pablofntdz/air-quality-scikit-learn"
 STATIONS = [4, 8, 11, 16, 17, 27, 35, 36, 38, 39, 40, 47, 48, 50, 56, 57, 60]
@@ -20,6 +21,7 @@ def download_models_if_missing():
         tmp_path = hf_hub_download(
             repo_id=REPO_ID,
             filename=f"models/{filename}",
+            token=os.getenv("HF_TOKEN"),
         )
         shutil.copy(tmp_path, local_path)
         print(f"[OK] {filename} → {local_path}")
